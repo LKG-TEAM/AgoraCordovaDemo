98faa3691878617348857b4e9082584cb1718603
潘涛 <pant@linkstec.com>
2020-11-23 17:52:26 +0800

    更新

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "CDVYogaAgoraRTC.CDVYogaAgoraRTC",
+      "file": "plugins/CDVYogaAgoraRTC/www/CDVYogaAgoraRTC.js",
+      "pluginId": "CDVYogaAgoraRTC",
+      "clobbers": [
+        "YogaAgoraRTC"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "CDVYogaAgoraRTC": "0.0.1"
+  };
+});
